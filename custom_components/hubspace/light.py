990f4ff89a6bb25a88fdad62d5de2c070e2ba413
jdeath <17914369+jdeath@users.noreply.github.com>
2022-06-15 18:23:52 -0400

    Add files via upload

--- custom_components/hubspace/light.py
+++ custom_components/hubspace/light.py
@@ -124,6 +124,7 @@ class HubspaceLight(LightEntity):
         if self._model == '50291, 50292':
             #self._useColorOrWhite = True
             self._usePowerFunctionInstance = 'light-power'
+            self._useBrightness = True
         
         # https://www.homedepot.com/p/Commercial-Electric-5-in-6-in-Smart-Hubspace-Color-Selectable-CCT-Integrated-LED-Recessed-Light-Trim-Works-with-Amazon-Alexa-and-Google-538561010/314254248
         if self._model == '538551010, 538561010, 538552010, 538562010':
