4cdc77bb683ab2cb006f4dbac85e553d226fed90
jdeath <17914369+jdeath@users.noreply.github.com>
2024-06-05 21:24:22 -0400

    Update light.py

--- custom_components/hubspace/light.py
+++ custom_components/hubspace/light.py
@@ -418,7 +418,7 @@ class HubspaceLight(LightEntity):
         # fan
         # https://www.homedepot.com/p/Home-Decorators-Collection-Driskol-60-in-White-Color-Changing-LED-Matte-Black-Smart-Ceiling-Fan-with-Light-Kit-and-Remote-Powered-by-Hubspace-56052/319830774
         # https://www.homedepot.com/p/Home-Decorators-Collection-Vinwood-56-in-Indoor-White-Color-Changing-LED-Brushed-Nickel-Smart-Hubspace-Ceiling-Fan-with-Remote-Control-56002/320816365
-        if self._model == "52133, 37833" or self._model == "76278, 37278" or self._model == "DriskolFan" or self._model == "VinwoodFan" or self._model == "CF2003":
+        if self._model == "52133, 37833" or self._model == "76278, 37278" or self._model == "DriskolFan" or self._model == "VinwoodFan" or self._model == "CF2003" or self._model == "NevaliFan":
             self._usePowerFunctionInstance = "light-power"
             self._supported_color_modes.extend([ColorMode.BRIGHTNESS])
             self._temperature_suffix = "K"
