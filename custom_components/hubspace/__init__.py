967057425190874277bcb765da410d11cb410923
cdohmen <chris.dohmen11@gmail.com>
2024-07-26 08:40:58 -0400

    Ability to remove devices
     * Add a delete button for a device that works when the device
       is not reported via the API
    
    Sem-Ver: feature

--- custom_components/hubspace/__init__.py
+++ custom_components/hubspace/__init__.py
@@ -7,12 +7,13 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
+from homeassistant.helpers.device_registry import DeviceEntry
 from hubspace_async import HubSpaceConnection
 
 from .const import UPDATE_INTERVAL_OBSERVATION
 from .coordinator import HubSpaceDataUpdateCoordinator
 
-logger = logging.getLogger(__name__)
+_LOGGER = logging.getLogger(__name__)
 
 PLATFORMS = [
     Platform.FAN,
@@ -57,3 +58,20 @@ async def async_setup_entry(hass: HomeAssistant, entry: HubSpaceData) -> bool:
 
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
+
+
+async def async_remove_config_entry_device(
+    hass: HomeAssistant, config_entry: HubSpaceConfigEntry, device_entry: DeviceEntry
+) -> bool:
+    """Remove a config entry from a device.
+
+    A device can only be removed if it is not present in the current API response
+    """
+    device_id = list(device_entry.identifiers)[0][1]
+    return not any(
+        [
+            x
+            for x in config_entry.runtime_data.coordinator_hubspace.tracked_devices
+            if x.device_id == device_id
+        ]
+    )
