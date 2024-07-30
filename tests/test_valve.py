0c25dc84f920649088cde9c56d416fc6e548e73c
cdohmen <chris.dohmen11@gmail.com>
2024-07-29 22:51:48 -0400

    Check-in valve UT
     * Add in UT for valve update_states
    
    Sem-Ver: bugfix

---
+++ tests/test_valve.py
@@ -0,0 +1,31 @@
+import pytest
+
+from custom_components.hubspace import valve
+from custom_components.hubspace.const import ENTITY_VALVE
+
+from .utils import create_devices_from_data
+
+
+@pytest.fixture
+def empty_valve(mocked_coordinator):
+    yield valve.HubSpaceValve(mocked_coordinator, "test valve", None)
+
+
+spigot = create_devices_from_data("water-timer.json")[0]
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "instance,states,expected_attrs",
+    [
+        ("spigot-1", spigot.states, {"_state": "off"}),
+        ("spigot-2", spigot.states, {"_state": "on"}),
+    ],
+)
+async def test_update_states(instance, states, expected_attrs, empty_valve):
+    empty_valve._instance = instance
+    empty_valve.states = states
+    empty_valve.coordinator.data[ENTITY_VALVE][empty_valve._child_id] = empty_valve
+    empty_valve.update_states()
+    for key, val in expected_attrs.items():
+        assert getattr(empty_valve, key) == val
