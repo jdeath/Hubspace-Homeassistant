39374be5e3247f871d0c213761401a946568ba75
Chris Dohmen <chris.dohmen11@gmail.com>
2025-01-12 20:48:46 -0500

    Fix fan action
     * Fix an issue where actions + fans could cause an UHE
    
    Sem-Ver: bugfix

--- tests/test_fan.py
+++ tests/test_fan.py
@@ -1,7 +1,8 @@
 import pytest
+from aiohubspace.v1.device import HubspaceState
 from homeassistant.helpers import entity_registry as er
 
-from .utils import create_devices_from_data
+from .utils import create_devices_from_data, modify_state
 
 fan_zandra = create_devices_from_data("fan-ZandraFan.json")
 fan_zandra_instance = fan_zandra[0]
@@ -64,6 +65,53 @@ async def test_turn_on(mocked_entity):
     assert update_call.args[0] == "put"
     payload = update_call.kwargs["json"]
     assert payload["metadeviceId"] == fan_zandra_instance.id
+
+
+@pytest.mark.asyncio
+async def test_turn_on_limited(mocked_entry):
+    hass, entry, bridge = mocked_entry
+    await bridge.fans.initialize_elem(exhaust_fan_instance)
+    await bridge.devices.initialize_elem(exhaust_fan[2])
+    bridge.fans._initialize = True
+    bridge.devices._initialize = True
+    bridge.fans._items[exhaust_fan_instance.id].on.on = False
+    await hass.config_entries.async_setup(entry.entry_id)
+    await hass.async_block_till_done()
+    await hass.services.async_call(
+        "fan",
+        "turn_on",
+        {"entity_id": exhaust_fan_instance_entity_id},
+        blocking=True,
+    )
+    update_call = bridge.request.call_args_list[-1]
+    assert update_call.args[0] == "put"
+    payload = update_call.kwargs["json"]
+    assert payload["metadeviceId"] == exhaust_fan_instance.id
+    update = payload["values"][0]
+    assert update["functionClass"] == "power"
+    assert update["functionInstance"] == "fan-power"
+    assert update["value"] == "on"
+    # Now generate update event by emitting the json we've sent as incoming event
+    exhaust_fan_update = create_devices_from_data("fan-exhaust-fan.json")[3]
+    modify_state(
+        exhaust_fan_update,
+        HubspaceState(
+            functionClass="power",
+            functionInstance="fan-power",
+            value="on",
+        ),
+    )
+    event = {
+        "type": "update",
+        "device_id": exhaust_fan_update.id,
+        "device": exhaust_fan_update,
+    }
+    bridge.emit_event("update", event)
+    await hass.async_block_till_done()
+    assert bridge.fans._items[exhaust_fan_update.id].on.on
+    test_entity = hass.states.get(exhaust_fan_instance_entity_id)
+    assert test_entity is not None
+    assert test_entity.state == "on"
 
 
 @pytest.mark.asyncio
