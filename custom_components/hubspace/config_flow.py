0d94627818d6389855990e002f8f54c9d48ee1ab
Chris Dohmen <chris.dohmen11@gmail.com>
2025-10-04 08:02:38 -0400

    Update config_flow to handle OTP workflow

--- custom_components/hubspace/config_flow.py
+++ custom_components/hubspace/config_flow.py
@@ -3,20 +3,26 @@
 from __future__ import annotations
 
 from asyncio import timeout
-import contextlib
+from collections.abc import Mapping
 import logging
-from typing import Any, NamedTuple
+from typing import Any
 
-from aioafero import InvalidAuth
+from aioafero import InvalidAuth, InvalidOTP, OTPRequired
 from aioafero.v1 import AferoBridgeV1
 from aioafero.v1.v1_const import AFERO_CLIENTS
-from homeassistant import config_entries
+from homeassistant.config_entries import (
+    ConfigEntry,
+    ConfigFlow,
+    ConfigFlowResult,
+    OptionsFlow,
+)
 from homeassistant.const import CONF_PASSWORD, CONF_TIMEOUT, CONF_TOKEN, CONF_USERNAME
 from homeassistant.core import callback
 import voluptuous as vol
 
 from .const import (
     CONF_CLIENT,
+    CONF_OTP,
     DEFAULT_POLLING_INTERVAL_SEC,
     DEFAULT_TIMEOUT,
     DOMAIN,
@@ -32,11 +38,12 @@ LOGIN_REQS = {
     vol.Required(CONF_PASSWORD): str,
     vol.Required(CONF_CLIENT): vol.In(sorted(AFERO_CLIENTS.keys())),
 }
-REAUTH = vol.Schema(
-    {
-        vol.Required(CONF_PASSWORD): str,
-    }
-)
+OTP_REQS = {
+    vol.Required(CONF_OTP): str,
+}
+REAUTH_REQS = {
+    vol.Required(CONF_PASSWORD): str,
+}
 OPTIONAL = {
     vol.Required(CONF_TIMEOUT): int,
     vol.Required(POLLING_TIME_STR): int,
@@ -44,161 +51,184 @@ OPTIONAL = {
 LOGIN_SCHEMA = vol.Schema(LOGIN_REQS | OPTIONAL)
 RECONFIG_SCHEMA = vol.Schema(OPTIONAL)
 
-
-class AuthResult(NamedTuple):
-    """Class to store authentication result data.
-
-    Attributes:
-        token: Authentication token string obtained after successful login
-        err_type: Error type string if authentication failed, None if successful
-
-    """
-
-    token: str
-    err_type: str
+BRIDGE_INSTANCE: dict[str, AferoBridgeV1] = {}
 
 
-async def validate_auth(user_input: dict[str, Any] | None = None) -> AuthResult | None:
-    """Validate and save auth."""
-    err_type = None
-    bridge = AferoBridgeV1(
-        user_input[CONF_USERNAME],
-        user_input[CONF_PASSWORD],
-        afero_client=user_input[CONF_CLIENT],
-    )
-    try:
-        async with timeout(user_input[CONF_TIMEOUT] / 1000):
-            await bridge.get_account_id()
-    except TimeoutError:
-        err_type = "cannot_connect"
-    except InvalidAuth:
-        err_type = "invalid_auth"
-    except Exception:
-        _LOGGER.exception("Unexpected exception")
-        err_type = "unknown"
-    finally:
-        await bridge.close()
-    return AuthResult(bridge.refresh_token, err_type)
-
-
-class HubspaceConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Hubspace."""
+class AferoConfigFlow(ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Afero."""
 
     VERSION = const_maj
     MINOR_VERSION = const_min
-    username: str
-    password: str
-    conn: AferoBridgeV1
 
-    @staticmethod
-    def extract_user_data(user_input: dict[str, Any] | None) -> tuple[dict, dict]:
-        """Extract user data and options from input dictionary.
+    def __init__(self) -> None:
+        """Initialize."""
+        self._conn: AferoBridgeV1 | None = None
+        self._otp_code: str | None = None
+        self._username: str | None = None
+        self._password: str | None = None
+        self._polling: int | None = DEFAULT_POLLING_INTERVAL_SEC
+        self._timeout: int | None = DEFAULT_TIMEOUT
+        self._client: str | None = None
 
-        Args:
-            user_input: Dictionary containing user input data and options.
+    async def _async_afero_login(
+        self, step_id: str, schema: vol.Schema
+    ) -> ConfigFlowResult:
+        """Validate the Afero username and password."""
+        errors = {}
+        self._conn = AferoBridgeV1(
+            self._username,
+            self._password,
+            afero_client=self._client,
+            client_name="Home Assistant",
+        )
+        try:
+            async with timeout(self._timeout):
+                await self._conn.get_account_id()
+        except TimeoutError:
+            errors = {"base": "cannot_connect"}
+        except InvalidAuth:
+            errors = {"base": "invalid_auth"}
+        except OTPRequired:
+            return await self.async_step_otp()
+        except Exception:
+            _LOGGER.exception("Unexpected exception")
+            errors = {"base": "unknown"}
+        if errors:
+            return self.async_show_form(
+                step_id=step_id, data_schema=schema, errors=errors
+            )
+        return await self._async_create_entry()
 
-        Returns:
-            A tuple containing:
-                - data: Dictionary with non-option user input values
-                - options: Dictionary with option values
+    async def _async_afero_otp(self) -> ConfigFlowResult:
+        """Handle the OTP step for Afero."""
+        try:
+            async with timeout(self._timeout):
+                await self._conn.otp_login(self._otp_code)
+        except InvalidOTP:
+            return self.async_show_form(
+                step_id="otp",
+                data_schema=vol.Schema(OTP_REQS),
+                errors={"base": "invalid_otp"},
+            )
+        except Exception:
+            _LOGGER.exception("Unexpected exception")
+            return self.async_show_form(
+                step_id="otp",
+                data_schema=vol.Schema(OTP_REQS),
+                errors={"base": "unknown_otp"},
+            )
+        return await self._async_create_entry()
 
-        """
-        options = {}
-        data = {}
-        importable_options = [CONF_TIMEOUT, POLLING_TIME_STR]
-        for key in user_input:
-            if key in importable_options:
-                options[key] = user_input[key]
-            else:
-                data[key] = user_input[key]
-        return data, options
+    async def _async_create_entry(self) -> ConfigFlowResult:
+        """Create the config entry."""
+        unique_id = self._username.lower()
+        existing_entry = await self.async_set_unique_id(unique_id)
+        data = {
+            CONF_USERNAME: self._username,
+            CONF_PASSWORD: self._password,
+            CONF_CLIENT: self._client,
+            CONF_TOKEN: self._conn.refresh_token,
+        }
+        options = {
+            CONF_TIMEOUT: self._timeout or DEFAULT_TIMEOUT,
+            POLLING_TIME_STR: self._polling or DEFAULT_POLLING_INTERVAL_SEC,
+        }
+        if existing_entry:
+            return self.async_update_reload_and_abort(
+                existing_entry, data=data, options=options
+            )
+        return self.async_create_entry(
+            title=unique_id,
+            data=data,
+            options=options,
+        )
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
-    ) -> config_entries.ConfigFlowResult | dict:
+    ) -> ConfigFlowResult:
         """Handle a flow initialized by the user."""
-        errors: dict[str, str] = {}
-        if user_input is not None:
-            user_input[CONF_TIMEOUT] = user_input.get(CONF_TIMEOUT) or DEFAULT_TIMEOUT
-            user_input[POLLING_TIME_STR] = user_input.get(POLLING_TIME_STR) or 30
-            unique_id: str = user_input[CONF_USERNAME].lower()
-            await self.async_set_unique_id(unique_id, raise_on_progress=False)
-            data, options = self.extract_user_data(user_input)
-            if (
-                self.source == config_entries.SOURCE_REAUTH
-                and self._get_reauth_entry().unique_id != self.unique_id
-            ):
-                errors["base"] = "unique_id_mismatch"
-                return self.async_show_form(
-                    step_id="user",
-                    data_schema=LOGIN_SCHEMA,
-                    errors=errors,
-                )
-            auth_data = await validate_auth(user_input)
-            if not auth_data.err_type:
-                data[CONF_TOKEN] = auth_data.token
-                if user_input[POLLING_TIME_STR] < 2:
-                    errors["base"] = "polling_too_short"
-                elif self.source == config_entries.SOURCE_REAUTH:
-                    return self.async_update_reload_and_abort(
-                        self._get_reauth_entry(),
-                        data_updates=data,
-                        options=options,
-                    )
-                else:
-                    self._abort_if_unique_id_configured(reload_on_update=True)
-                    return self.async_create_entry(
-                        title=unique_id, data=data, options=options
-                    )
-            else:
-                errors["base"] = auth_data.err_type
-        with contextlib.suppress(Exception):
-            await self.bridge.close()
-        return self.async_show_form(
-            step_id="user",
-            data_schema=LOGIN_SCHEMA,
-            errors=errors,
-        )
+        if user_input is None:
+            return self.async_show_form(
+                step_id="user",
+                data_schema=LOGIN_SCHEMA,
+            )
+        try:
+            user_input.update(validate_options(user_input))
+        except ValueError as err:
+            return self.async_show_form(
+                step_id="user",
+                data_schema=LOGIN_SCHEMA,
+                errors={"base": str(err)},
+            )
+        self._username = user_input[CONF_USERNAME]
+        self._password = user_input[CONF_PASSWORD]
+        self._client = user_input[CONF_CLIENT]
+        self._timeout = user_input[CONF_TIMEOUT]
+        self._polling = user_input[POLLING_TIME_STR]
+        return await self._async_afero_login("user", LOGIN_SCHEMA)
+
+    async def async_step_otp(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Handle an OTP flow."""
+        if user_input is None:
+            return self.async_show_form(step_id="otp", data_schema=vol.Schema(OTP_REQS))
+        self._otp_code = user_input[CONF_OTP]
+        return await self._async_afero_otp()
 
     async def async_step_reauth(
-        self, user_input: dict[str, Any] | None = None
-    ) -> config_entries.ConfigFlowResult:
-        """Handle a reauth flow."""
+        self, entry_data: Mapping[str, Any]
+    ) -> ConfigFlowResult:
+        """Handle reauthorization request from Afero."""
+        current = self._get_reauth_entry()
+        self._username = current.data[CONF_USERNAME]
+        self._password = current.data[CONF_PASSWORD]
+        self._client = current.data[CONF_CLIENT]
+        self._timeout = current.options.get(CONF_TIMEOUT, DEFAULT_TIMEOUT)
+        self._polling = current.options.get(
+            POLLING_TIME_STR, DEFAULT_POLLING_INTERVAL_SEC
+        )
         return await self.async_step_reauth_confirm()
 
     async def async_step_reauth_confirm(
         self, user_input: dict[str, Any] | None = None
-    ) -> config_entries.ConfigFlowResult:
-        """Dialog that informs the user that reauth is required."""
-        if user_input is None:
+    ) -> ConfigFlowResult:
+        """Handle reauthorization flow."""
+        reauth_schema = vol.Schema(
+            {
+                vol.Required(CONF_PASSWORD): str,
+            }
+        )
+        if not user_input:
             return self.async_show_form(
                 step_id="reauth_confirm",
-                data_schema=vol.Schema({}),
+                data_schema=reauth_schema,
             )
-        return await self.async_step_user()
+        self._password = user_input[CONF_PASSWORD]
+        return await self._async_afero_login("reauth_confirm", reauth_schema)
 
     @staticmethod
     @callback
     def async_get_options_flow(
-        config_entry: config_entries.ConfigEntry,
-    ) -> HubspaceOptionsFlowHandler:
+        config_entry: ConfigEntry,
+    ) -> AferoOptionsFlowHandler:
         """Get the options flow for this handler."""
-        return HubspaceOptionsFlowHandler()
+        return AferoOptionsFlowHandler()
 
 
-class HubspaceOptionsFlowHandler(config_entries.OptionsFlow):
-    """Handle options config flow for Hubspace."""
+class AferoOptionsFlowHandler(OptionsFlow):
+    """Handle options config flow for Afero."""
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
-    ) -> config_entries.ConfigFlowResult:
+    ) -> ConfigFlowResult:
         """Handle UI Options workflow."""
         errors: dict[str, str] = {}
         if user_input is not None:
-            if user_input[POLLING_TIME_STR] == 0:
-                user_input[POLLING_TIME_STR] = DEFAULT_POLLING_INTERVAL_SEC
-            if user_input[POLLING_TIME_STR] < 2:
-                errors["base"] = "polling_too_short"
+            try:
+                user_input.update(validate_options(user_input))
+            except ValueError as err:
+                errors["base"] = str(err)
             if not errors:
                 return self.async_create_entry(data=user_input)
             errors["base"] = "polling_too_short"
@@ -216,3 +246,15 @@ class HubspaceOptionsFlowHandler(config_entries.OptionsFlow):
             ),
             errors=errors,
         )
+
+
+def validate_options(user_input: dict[str, Any]) -> dict[str, str]:
+    """Validate the user input allows us to connect."""
+    validated = {
+        POLLING_TIME_STR: user_input.get(POLLING_TIME_STR, DEFAULT_POLLING_INTERVAL_SEC)
+        or DEFAULT_POLLING_INTERVAL_SEC,
+        CONF_TIMEOUT: user_input.get(CONF_TIMEOUT, DEFAULT_TIMEOUT) or DEFAULT_TIMEOUT,
+    }
+    if validated[POLLING_TIME_STR] < 2:
+        raise ValueError("polling_too_short")
+    return validated
